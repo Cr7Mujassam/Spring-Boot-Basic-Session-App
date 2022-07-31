5684a1faba9107d6a77f4d62772b90b7b40061b8
Cr7Mujassam <106069072+Cr7Mujassam@users.noreply.github.com>
2022-07-31 22:54:47 +0530

    Add files via upload

---
+++ ListShow.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.web.springbootfirstwebapplication.list;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListShow {
+	
+	public ArrayList<String> showList() {
+		ArrayList<String> li =  new ArrayList<>();
+		li.add("Honda City");
+		li.add("Tesla");
+		li.add("Mustang GT");
+		
+		return li;
+		
+	}
+}
---
+++ list.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>List of cars</title>
+</head>
+
+<body>
+List of Cars ${list} You are ${object}
+</body>
+</html>
